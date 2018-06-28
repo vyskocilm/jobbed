5ee779532e572823fd57ce6f5f96049266223bd9
Michal Vyskocil <michal.vyskocil@gmail.com>
2018-06-28 10:22:05 +0200

    Problem: no way to discover scripts
    Solution: hack up Script class taking a care about it + add GET for
    /scripts/<script>, which return the basic information about script

--- rest/app.py
+++ rest/app.py
@@ -6,6 +6,44 @@ from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
+def call_pdflatex ():
+    pass
+
+def call_gsl ():
+    pass
+
+def make_jresponse (obj, return_code=http.HTTPStatus.OK):
+    return make_response (jsonify (obj), return_code)
+
+class Scripts:
+
+    def __init__ (self):
+        self._scripts = {
+            "latex" : 
+                {"path" : "../jobbed_latex.gsl",
+                 "description" : "LaTeX output",
+                 "code" : call_pdflatex},
+            "html" :
+                {"path" : "../jobbed_html.gsl",
+                 "description" : "plain HTML5 output",
+                 "code" : call_gsl},
+        }
+
+        self._rest = [{"script" : script, "description" : value ["description"]} for
+                script, value in self._scripts.items ()]
+
+    @property
+    def rest (self):
+        return self._rest
+
+    def __in__ (self, script):
+        return script in self._scripts
+
+    def __getitem__ (self, script):
+        return self._scripts [script]
+
+SCRIPTS = Scripts ()
+
 @app.route('/')
 def index():
     return """
@@ -32,16 +70,29 @@ def not_found(error):
 @app.route ("/api/v0.1/scripts", methods=["GET"])
 def get_scripts ():
     """Return the list of resume scripts available on the system"""
+
+    global SCRIPTS
     return make_response (
-        jsonify ({"scripts": [
-            {"script" : "jobbed_html", "Content-type" : "text/html; charset=utf-8"},
-            {"script" : "jobbed_latex", "Content-type" : "text/plain; charset=utf-8"}]}),
-        http.HTTPStatus.NOT_IMPLEMENTED)
+            jsonify ({"scripts": SCRIPTS.rest}),
+            http.HTTPStatus.OK)
+
+@app.route ("/api/v0.1/scripts/<script>", methods=["GET"])
+def get_scripts_script (script):
+
+    global SCRIPTS
+    try:
+        return make_jresponse (
+            {"script" : script,
+             "description": SCRIPTS [script]["description"]})
+    except KeyError:
+        return make_jresponse (
+            {"error" : "Not found script '%s'" % script},
+            http.HTTPStatus.NOT_FOUND)
 
 
 @app.route ("/api/v0.1/scripts/<script>", methods=["POST"])
 def post_resume (script):
-    # 1. get the 
+    global SCRIPTS
     return make_response (
             jsonify ({"error" : "Not yet implemented: " + script}),
         http.HTTPStatus.NOT_IMPLEMENTED)
